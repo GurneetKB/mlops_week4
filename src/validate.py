703d990cf86ad7dd97eabd6e72fa9a9ca3ed7e23
Gurneet Kaur Bhuller <gurneetbhuller02@gmail.com>
2025-06-29 22:40:24 +0530

    file changes

--- src/validate.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-def validate():
-    df = pd.read_csv("data/iris.csv")
-    assert df.shape[1] == 5, "Dataset should have 5 columns"
-    assert df.isnull().sum().sum() == 0, "Dataset should not have nulls"
-
-if __name__ == "__main__":
-    validate()
