6514b489761b0c5d28f20a663680b692f2629ed5
Gurneet Kaur Bhuller <gurneetbhuller02@gmail.com>
2025-06-29 22:23:33 +0530

    Initial commit - mlops week 4 demo  structure

---
+++ tests/test_validation.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+def test_data_shape():
+    df = pd.read_csv("data/iris.csv")
+    assert df.shape[1] == 5
+
+def test_no_nulls():
+    df = pd.read_csv("data/iris.csv")
+    assert df.isnull().sum().sum() == 0
